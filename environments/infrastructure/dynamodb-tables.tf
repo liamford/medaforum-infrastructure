d3f635f3d4c33c16596b871fad8d788e431482aa
Liam Ford <liam.ford.au@gmail.com>
2020-06-27 14:54:42 +1000

    added Dynamodb table - questions

--- environments/infrastructure/dynamodb-tables.tf
+++ environments/infrastructure/dynamodb-tables.tf
@@ -1,10 +1,25 @@
 resource "aws_dynamodb_table" "basic-dynamodb-table" {
-    name           = "GameScores"
+    name           = "Questions"
     billing_mode   = "PROVISIONED"
     read_capacity  = 2
     write_capacity = 2
-    hash_key       = "UserId"
-    range_key      = "GameTitle"
+    hash_key       = "QuestionId"
+    range_key      = "UserId"
+
+    attribute {
+        name = "QuestionId"
+        type = "S"
+    }
+
+    attribute {
+        name = "QuestionTitle"
+        type = "S"
+    }
+
+    attribute {
+        name = "Timestamp"
+        type = "S"
+    }
 
     attribute {
         name = "UserId"
@@ -12,28 +27,50 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
     }
 
     attribute {
-        name = "GameTitle"
-        type = "S"
-    }
-
-    attribute {
-        name = "TopScore"
+        name = "Status"
         type = "N"
     }
 
-    ttl {
-        attribute_name = "TimeToExist"
-        enabled        = true
+    attribute {
+        name = "QuestionGroup"
+        type = "N"
     }
 
+    local_secondary_index {
+        name = "QuestionTitleIndex"
+        projection_type = "ALL"
+        range_key = "QuestionTitle"
+    }
+
+
     global_secondary_index {
-        name               = "GameTitleIndex"
-        hash_key           = "GameTitle"
-        range_key          = "TopScore"
+        name               = "QuestionStatusIndex"
+        hash_key           = "Status"
+        range_key          = "Timestamp"
         write_capacity     = 2
         read_capacity      = 2
         projection_type    = "INCLUDE"
-        non_key_attributes = ["UserId"]
+        non_key_attributes = ["QuestionId","QuestionTitle","UserId"]
+    }
+
+    global_secondary_index {
+        name               = "QuestionGroupIndex"
+        hash_key           = "QuestionGroup"
+        range_key          = "Timestamp"
+        write_capacity     = 2
+        read_capacity      = 2
+        projection_type    = "INCLUDE"
+        non_key_attributes = ["QuestionId","QuestionTitle","UserId","Status"]
+    }
+
+    global_secondary_index {
+        name               = "QuestionUserIndex"
+        hash_key           = "UserId"
+        range_key          = "Status"
+        write_capacity     = 2
+        read_capacity      = 2
+        projection_type    = "INCLUDE"
+        non_key_attributes = ["QuestionId","Timestamp","QuestionTitle"]
     }
 
     tags = {
