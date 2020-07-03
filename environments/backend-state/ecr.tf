c5bf0496c96c63624477a8c4520bd6568710da6d
Liam Ford <liam.ford.au@gmail.com>
2020-07-03 17:51:43 +1000

    added new ECR for sms

--- environments/backend-state/ecr.tf
+++ environments/backend-state/ecr.tf
@@ -1,5 +1,11 @@
-module "ecr" {
+module "general_ecr" {
     source = "QuiNovas/ecr/aws"
     version = "3.0.0"
     name = "medaforum"
+}
+
+module "sms_ecr" {
+    source = "QuiNovas/ecr/aws"
+    version = "3.0.0"
+    name = "sms"
 }
--- environments/backend-state/outputs.tf
+++ environments/backend-state/outputs.tf
@@ -1,4 +1,8 @@
 
 output "ecr-general_url" {
-    value = module.ecr.repository_url
+    value = module.general_ecr.repository_url
+}
+
+output "ecr-sms_url" {
+    value = module.sms_ecr.repository_url
 }
